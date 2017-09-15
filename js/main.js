5cc0c8c7a421ac65f8deeeb458cc461cbbd107b0
sbaxter95 <sbaxter95@outlook.com>
2017-09-15 16:55:44 +0100

    can win and display correct letters

--- js/main.js
+++ js/main.js
@@ -2,29 +2,45 @@ var planetsArray = ['tatooine', 'hoth', 'endor', 'alderaan', 'naboo', 'jakku'];
 
 var isMatch = false;
 
-var chosenWord = planetsArray[1];
+var chosenWord = planetsArray[4];
 
-var dash = "";
+var dash = [];
 
 for (var i = 0; i < chosenWord.length; i++) {
 	if (chosenWord.charAt(i) === " ") {
-		dash += " ";
+		dash.push (" ");
 	} else {
-		dash += " _ ";
+		dash.push(" _ ");
 	}
 }
 
-$('.word-container').html(dash);
+display();
 
-$('p').click(function (event) {
+function display () {
+	$('.word-container').html(dash);
+}
+
+$('p').one('click', function (event) {
 	var chosenLetter = $(this).attr('id');
 	console.log(chosenLetter);
+	var remainingLetters = chosenWord.length;
 	for (var i = 0; i < chosenWord.length; i++) {
 	if (chosenLetter === chosenWord[i]) {
 		isMatch = true;
-		console.log(isMatch);
+		console.log(remainingLetters);
+		var position = i;
+		dash[i] = chosenLetter;
+		display();
+		if (remainingLetters === 0) {
+		console.log('You won!');
+	}
+	} else {
+		isMatch  = false;
+		console.log('That letter is not in the word');
+		//Hangman stuff
 	}
 }
+
 	
 });
 
