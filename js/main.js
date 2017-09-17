78d4216ef8aba61510fd1c63b95d9506495ac2ee
sbaxter95 <sbaxter95@outlook.com>
2017-09-17 23:01:42 +0100

    minor modifications, testing out methods of gaining functionality

--- js/main.js
+++ js/main.js
@@ -27,37 +27,69 @@ function display () {
 }
 
 function draw (wrongLetters) {
+	// switch (wrongLetters){
+	// 	case 1:
+	// 		$('#gallows-1').addClass('show');
+	// 		break
+	// 	case 2:
+	// 		$('#gallows-2').addClass('show');
+	// 		break;
+	// 	case 3:
+	// 		$('#gallows-3').addClass('show');
+	// 		break;
+	// 	case 4:
+	// 		$('#head').addClass('show');
+	// 		break;
+	// }
 	if (wrongLetters === 1) {
-	        
+	        $('#gallows-1').addClass('show');
     	}
+    if (wrongLetters === 2) {
+    	$('#gallows-2').addClass('show');
+    } 
+    if (wrongLetters === 3) {
+    	$('#gallows-3').addClass('show');
+    } 
+    if (wrongLetters === 4) {
+    	$('#head').addClass('show');
+    }
 }
 
 $('p').one('click', function (event) {
 	clicks++;
 	var chosenLetter = $(this).attr('id');
-	console.log(chosenLetter);
 	for (var i = 0; i < chosenWord.length; i++) {
-	if (chosenLetter === chosenWord[i]) {
-		isMatch = true;
-		$(this).addClass('right-letter');
-		$(this).fadeOut(1000);
-		var position = i;
-		dash[i] = chosenLetter;
-		display();
-	} else {
-		isMatch  = false;
-		badGuesses.push(chosenLetter);
-		if (badGuesses.length === chosenWord.length) {
-			wrongLetters++;
-		}
-		$(this).addClass('wrong-letter');
-		$(this).fadeOut(1000);
-		draw(wrongLetters);
-	}
+		if (chosenLetter === chosenWord[i]) {
+			isMatch = true;
+			var goodGuess = true;
+			console.log(isMatch);
+			$(this).addClass('right-letter');
+			$(this).fadeOut(1000);
+			var position = i;
+			dash[i] = chosenLetter;
+			display();
+		} else if (goodGuess !== true) {
+				return isMatch = false;
+			}
+			// isMatch = false;
+			// badGuesses.push(chosenLetter);
+			// console.log(badGuesses);
+			// if (badGuesses.length === chosenWord.length) {
+			// 	wrongLetters++;
+			// 	draw(wrongLetters);
+			// 	console.log(wrongLetters);
+			// }
+			$(this).addClass('wrong-letter');
+			$(this).fadeOut(1000);
 }
 
 if ($('.word-container').html() === chosenWord) {
 		$('.result-container').html('You won!');
+}
+
+if (isMatch === false) {
+	wrongLetters++;
+	draw(wrongLetters);
 }
 
 // console.log(clicks);
